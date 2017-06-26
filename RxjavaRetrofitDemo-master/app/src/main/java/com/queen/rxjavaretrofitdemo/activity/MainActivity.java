91e2cbda773d9ac9cb269c9a9945aa61fbc74764
huangjiajie <huangjiajie@mingbikes.com>
2017-06-26 17:57:21 +0800

    源码的第一次提交

---
+++ RxjavaRetrofitDemo-master/app/src/main/java/com/queen/rxjavaretrofitdemo/activity/MainActivity.java
@@ -0,0 +1,64 @@
+package com.queen.rxjavaretrofitdemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.queen.rxjavaretrofitdemo.R;
+import com.queen.rxjavaretrofitdemo.subscribers.ProgressSubscriber;
+import com.queen.rxjavaretrofitdemo.subscribers.SubscriberOnNextListener;
+import com.queen.rxjavaretrofitdemo.entity.Subject;
+import com.queen.rxjavaretrofitdemo.http.HttpMethods;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.click_me_BN)
+    Button clickMeBN;
+    @Bind(R.id.result_TV)
+    TextView resultTV;
+
+    private SubscriberOnNextListener getTopMovieOnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getTopMovieOnNext = new SubscriberOnNextListener<List<Subject>>() {
+            @Override
+            public void onNext(List<Subject> subjects) {
+                resultTV.setText(subjects.toString());
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @OnClick(R.id.click_me_BN)
+    public void onClick() {
+        getMovie();
+    }
+
+    //进行网络请求
+    private void getMovie(){
+        HttpMethods.getInstance().getTopMovie(new ProgressSubscriber(getTopMovieOnNext, MainActivity.this), 0, 10);
+    }
+}
---
+++ RxjavaRetrofitDemo-master/app/src/main/java/com/queen/rxjavaretrofitdemo/http/MovieService.java
@@ -0,0 +1,28 @@
+package com.queen.rxjavaretrofitdemo.http;
+
+import com.queen.rxjavaretrofitdemo.entity.HttpResult;
+import com.queen.rxjavaretrofitdemo.entity.Subject;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Created by liukun on 16/3/9.
+ */
+public interface MovieService {
+
+//    @GET("top250")
+//    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+//    @GET("top250")
+//    Observable<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+//    @GET("top250")
+//    Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+    @GET("top250")
+    Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
+}
